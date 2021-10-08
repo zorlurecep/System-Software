4f345c92ba1559f4a38734f3dafe8cf50ac7312f
roza <recepomer.zorlu@student.kuleuven.be>
2021-10-08 08:53:53 +0200

    first part is done

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 ex1: main.c
 	mkdir -p build
-	gcc -Wall -Werror -o ./build/ex1 main.c
+	gcc -Wall -Werror -o ./build/ex1 ./ex1/main.c
 	./build/ex1
+
+ex2: 
+	gcc -Wall -Werror -0 ex2 ./ex2/main.c
+	./ex2/ex2
 
 zip: main.c
 	zip lab1_ex1.zip main.c
--- ex2/main.c
+++ ex2/main.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 
 int main(){
-    printf("%lu", sizeof(int)); // %lu -> long unsigned integer
-    printf("%lu", sizeof(float));
+    char *pointer; //regardless of the type of it it has the same size
+
+    printf("int: ");
+    printf("%lu\n", sizeof(int)); // %lu -> long unsigned integer    
+    printf("float: ");
+    printf("%lu\n", sizeof(float));    
+    printf("double: ");
+    printf("%lu\n", sizeof(double));
+    printf("void: ");
+    printf("%lu\n", sizeof(void));
+    printf("pointer: ");
+    printf("%lu\n", sizeof(pointer));
     return 0;
 }
