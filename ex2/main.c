384e8b7fd526a15149862364b4c90ad64cd5d7b8
roza <recepomer.zorlu@student.kuleuven.be>
2021-10-08 09:06:51 +0200

    second exercise finished

--- ex2/main.c
+++ ex2/main.c
@@ -13,5 +13,19 @@ int main(){
     printf("%lu\n", sizeof(void));
     printf("pointer: ");
     printf("%lu\n", sizeof(pointer));
+
+    printf("Part2 ------------------------------------------\n");
+    printf("short int: ");
+    printf("%lu\n", sizeof(short int));
+    printf("int: ");
+    printf("%lu\n", sizeof(int));
+    printf("long int: ");
+    printf("%lu\n", sizeof(long int));
+
+    printf("Part3 ------------------------------------------\n");
+    int result = ((char)(-1));
+    printf("%d\n",result);
+    printf("if negative signed char used in this system if positive unsigned is used\n");
+
     return 0;
 }
