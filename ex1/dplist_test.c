f07af87d58786ae49be977dc09341876c09a28a3
roza <recepomer.zorlu@student.kuleuven.be>
2021-12-30 16:08:14 +0100

    ex1 implemented. I think it is working correct.

--- ex1/dplist_test.c
+++ ex1/dplist_test.c
@@ -7,40 +7,57 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void setup(void) {
+void setup(void)
+{
     // Implement pre-test setup
 }
 
-void teardown(void) {
+void teardown(void)
+{
     // Implement post-test teardown
 }
 START_TEST(test_ListFree)
-    {
-        // Test free NULL
-        dplist_t *list = NULL;
-        dpl_free(&list);
-        ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+{
+    // Test free NULL
+    dplist_t *list = NULL;
+    // dpl_free(&list);
+    // ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
 
-        // Test free empty list
-        list = dpl_create();
-        dpl_free(&list);
-        ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
-        // TODO : Test free with one element
-
-        // TODO : Test free with multiple element
-
-    }
+    // Test free empty list
+    list = dpl_create();
+    dpl_free(&list);
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    // TODO : Test free with one element
+    list = dpl_create();
+    list = dpl_insert_at_index(list, 'A', 0);
+    list = dpl_insert_at_index(list, 'B', 1);
+    list = dpl_insert_at_index(list, 'C', 2);
+    list = dpl_insert_at_index(list, 'D', 3);
+    dpl_free(&list);
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    // TODO : Test free with multiple element
+}
 END_TEST
 
 START_TEST(test_ListInsertAtIndexListNULL)
-    {
-        // Test inserting at index -1
-        dplist_t *result = dpl_insert_at_index(NULL, 'A', -1);
-        ck_assert_msg(result == NULL, "Failure: expected list to be NULL");
-        // TODO : Test inserting at index 0
+{
+    dplist_t *list = dpl_create();
 
-        // TODO : Test inserting at index 99
-    }
+    // Test inserting to NULL list
+    // list = dpl_insert_at_index(NULL, 'A', 0);
+    // ck_assert_msg(list == NULL, "Failure: expected list to be NULL\n");
+
+    // Test inserting at index -1
+    // list = dpl_insert_at_index(list, 'A', -1);
+    // ck_assert_msg(list != NULL, "Failure: list should be poiting to a node\n");
+
+    // Test inserting at index 0
+    list = dpl_insert_at_index(list, 'A', 0);
+    ck_assert_msg(list != NULL, "Failure: list should be poiting to a node\n");
+    // Test inserting at index 99
+    list = dpl_insert_at_index(list, 'B', 99);
+    ck_assert_msg(list != NULL, "Failure: list should be poiting to a node\n");
+}
 END_TEST
 
 START_TEST(test_ListInsertAtIndexListEmpty)
@@ -49,7 +66,7 @@ START_TEST(test_ListInsertAtIndexListEmpty)
     dplist_t *list = dpl_create();
     dplist_t *result = dpl_insert_at_index(list, 'A', -1);
     ck_assert_msg(dpl_size(result) == 1, "Failure: expected list to have size of 1, got a size of %d",
-                                         dpl_size(result));
+                  dpl_size(result));
     dpl_free(&list);
     // TODO : Test inserting at index 0
 
@@ -57,11 +74,12 @@ START_TEST(test_ListInsertAtIndexListEmpty)
 }
 END_TEST
 
-//START_TEST(test_nameOfYourTest)
-//  Add other testcases here...
-//END_TEST
+// START_TEST(test_nameOfYourTest)
+//   Add other testcases here...
+// END_TEST
 
-int main(void) {
+int main(void)
+{
     Suite *s1 = suite_create("LIST_EX1");
     TCase *tc1_1 = tcase_create("Core");
     SRunner *sr = srunner_create(s1);
@@ -70,9 +88,9 @@ int main(void) {
     suite_add_tcase(s1, tc1_1);
     tcase_add_checked_fixture(tc1_1, setup, teardown);
     tcase_add_test(tc1_1, test_ListFree);
-    tcase_add_test(tc1_1, test_ListInsertAtIndexListNULL);
-    tcase_add_test(tc1_1, test_ListInsertAtIndexListEmpty);
-    // Add other tests here...
+    // tcase_add_test(tc1_1, test_ListInsertAtIndexListNULL);
+    //  tcase_add_test(tc1_1, test_ListInsertAtIndexListEmpty);
+    //   Add other tests here...
 
     srunner_run_all(sr, CK_VERBOSE);
 
---
+++ ex1/main.c
@@ -0,0 +1,78 @@
+#include "dplist.h"
+#include <stdio.h>
+
+void test_insert()
+{
+    dplist_t *list = dpl_create();
+    list = dpl_insert_at_index(list, 'A', 0);
+    list = dpl_insert_at_index(list, 'B', 1);
+    list = dpl_insert_at_index(list, 'C', 2);
+    list = dpl_insert_at_index(list, 'D', 3);
+    list = dpl_insert_at_index(list, 'E', 4);
+    list = dpl_insert_at_index(list, 'F', 5);
+    list = dpl_insert_at_index(list, 'G', 6);
+
+    // Insert to beginning
+    list = dpl_insert_at_index(list, 'X', 0);
+    // Insert to middle
+    list = dpl_insert_at_index(list, 'Y', 3);
+    // Insert to end
+    list = dpl_insert_at_index(list, 'Z', dpl_size(list));
+
+    dpl_free(&list);
+
+    printf("\ntest insert finished\n");
+}
+
+void test_remove(){
+    dplist_t *list = dpl_create();
+    list = dpl_insert_at_index(list, 'A', 0);
+    list = dpl_insert_at_index(list, 'B', 1);
+    list = dpl_insert_at_index(list, 'C', 2);
+    list = dpl_insert_at_index(list, 'D', 3);
+
+    //beginning
+    list = dpl_remove_at_index(list, 0);
+    list = dpl_insert_at_index(list, 'A', 0);
+    // middle
+    list = dpl_remove_at_index(list, 1);
+    list = dpl_insert_at_index(list, 'B', 1);
+    // end
+    list = dpl_remove_at_index(list, 3);
+    list = dpl_insert_at_index(list, 'D', 3);
+
+    dpl_free(&list);    
+
+    printf("\ntest remove finished\n");
+}
+
+void test_element_operations(){
+    dplist_t *list = dpl_create();
+    list = dpl_insert_at_index(list, 'A', 0);
+    list = dpl_insert_at_index(list, 'B', 1);
+    list = dpl_insert_at_index(list, 'C', 2);
+    list = dpl_insert_at_index(list, 'D', 3);
+    list = dpl_insert_at_index(list, 'E', 4);
+    list = dpl_insert_at_index(list, 'F', 5);
+    list = dpl_insert_at_index(list, 'G', 6);
+
+    element_t cur_element = dpl_get_element_at_index(list, 4);
+    printf("current element is => %c\n", cur_element);
+
+    int index = dpl_get_element_at_index(list, cur_element);
+    printf("current element is at index => %d\n", index);
+
+    dpl_free(&list);    
+
+    printf("\ntest element operations finished\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    test_insert();
+
+    test_remove();
+
+    test_element_operations();
+    return 0;
+}
